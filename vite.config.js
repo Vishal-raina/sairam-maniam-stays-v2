7c1f1027643850b59c328ba91c5bffade7379fc8
Vishal K S <vishalrak000@gmail.com>
2026-08-11 11:26:30 +0530

    fix: deployment issue

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/sairam-maniam-stays-v2/',
   plugins: [react(), tailwindcss()],
 });
